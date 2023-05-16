63cb33384ebacbade492ec5c912de18a0e677d33
Kulpot <kulpotwahu@gmail.com>
2023-05-17 02:58:05 +0800

    Add project files.

---
+++ CheckFileExists/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Windows.Forms;
+
+namespace CheckFileExists
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            string path = @"C:\my.log";
+
+            if (!File.Exists(path))
+            {
+                MessageBox.Show("File Exists!");
+            }
+        }
+    }
+}
